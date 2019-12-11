3d4a6d3655dfacff3a65a841191e17cc71f9623a
Brian H. Ward <glyphrider@gmail.com>
2019-12-11 15:30:36 -0500

    Additional optimization by only testing prime factors (up to sqrt of the candidate, as determined by squaring the factor)

--- primes.c
+++ primes.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct factor_node {
+  int factor;
+  struct factor_node *next;
+} node;
 
 int parse_commandline(int argc, char *argv[])
 {
@@ -8,10 +14,26 @@ int parse_commandline(int argc, char *argv[])
   return result;
 }
 
-int is_prime(int candidate)
+int addnode(node **start, node **end, int value)
 {
-  for(int factor = 2; factor * factor <= candidate; factor++)
-    if(candidate % factor == 0)
+  if(*start)
+  {
+    (*end)->next = (node*)malloc(sizeof(node));
+    *end = (*end)->next;
+  }
+  else
+  {
+    *start = (node*)malloc(sizeof(node));
+    *end = *start;
+  }
+  (*end)->factor = value;
+  (*end)->next = 0;
+}
+
+int is_prime(int candidate,node *factors)
+{
+  for(;factors && factors->factor * factors->factor <= candidate; factors = factors->next)
+    if(candidate % factors->factor == 0)
       return 0;
   return 1;
 }
@@ -20,13 +42,19 @@ int main(int argc, char *argv[])
 {
   int count = 0;
   int candidate = 2;
+  node *factors;
+  node *last;
+
+  factors = 0;
+  last = 0;
 
   int number_to_find = parse_commandline(argc,argv);
 
   while(1)
   {
-    if(is_prime(candidate))
+    if(is_prime(candidate,factors))
     {
+      addnode(&factors,&last,candidate);
       fprintf(stdout,"%d is prime %d.\n",candidate,++count);
     }
     if(count == number_to_find) break;
